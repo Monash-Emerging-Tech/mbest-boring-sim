67561101b6a98e8932f8aa0402f53e1b48898766
Renee McMillan <r.mcmillan.tolley@gmail.com>
2025-04-16 14:29:56 +1000

    Disable now-unused animation script
    Was causing a teensy lil bug

--- Assets/AnimationInitialiser.cs
+++ Assets/AnimationInitialiser.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+/** Temporarily disabled as now animations given with Gaias new model as of 16/4/25 **/
+
 public class AnimationInitialiser : MonoBehaviour
 {
     public Animator rigAnimator;
